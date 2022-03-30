fedd9a551a2b7b2296af17767bc7822d7a81a71a
DavidJuarezVazquez199 <79029636+DavidJuarezVazquez199@users.noreply.github.com>
2022-03-30 16:33:39 -0600

    Delete condicionales directory

--- condicionales/condicion/if.js
+++
@@ -1,7 +0,0 @@
-alert("Bienvenido");
-var mensaje ="Te gustan las frutas?";
-var nombreUsuario=prompt(mensaje);
-if (nombreUsuario == "si") 
-alert("Tienes buenos gustos")
-else 
-alert("Derias probarlas")
