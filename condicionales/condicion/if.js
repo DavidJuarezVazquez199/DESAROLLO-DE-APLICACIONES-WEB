34668337e7e19b5bbe86b59b11db6053fde24b81
DavidJuarezVazquez199 <79029636+DavidJuarezVazquez199@users.noreply.github.com>
2022-03-30 16:49:03 -0600

    Condicionales con javascrip y html

---
+++ condicionales/condicion/if.js
@@ -0,0 +1,7 @@
+alert("Hola");
+var mensaje ="Te gustan el jamon?";
+var nombreUsuario=prompt(mensaje);
+if (nombreUsuario == "si") 
+alert("Que genial tambien a mi")
+else 
+alert("chales a mi si")
---
+++ condicionales/condicion/switch_case.js
@@ -0,0 +1,25 @@
+var opc=window.prompt("selecciona un numero del 1 al 6");
+switch (opc){
+    case('1'):
+    alert('elegiste la uno');
+        break;
+    case('2'):
+    alert('elegiste la dos');
+        break;
+    case('3'):
+    alert('elegiste la tres');
+        break;
+    case('4'):
+    alert('elegiste el cuatro');
+        break;
+    case('5'):
+    alert('elegiste el 5');
+        break;
+    case('6'):
+        alert('elegiste la seis');
+        break;
+    default:
+        alert('ese numero no existe');
+        break;
+}        
+    
